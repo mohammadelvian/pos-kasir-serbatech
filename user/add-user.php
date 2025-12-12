d8ed2efaa59eb16de91a05f583eff094ee8436e9
mohammadelvian <mohammadelvian@gmail.com>
2025-12-12 14:42:15 +0700

    modifikasi user add user.php

--- user/add-user.php
+++ user/add-user.php
@@ -31,62 +31,64 @@ require "../template/sidebar.php";
     <section class="content">
         <div class="container-fluid">
             <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title"><i class="fas fa-user-plus fa-sm"></i> Add User</h3>
-                    <button type="submit" name="simpan" class="btn btn-primary btn-sm float-right"><i class="far fa-save"></i> Simpan</button>
-                    <button type="reset" class="btn btn-danger btn-sm float-right mr-1"><i class="fas fa-times"></i> Reset</button>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-8 mb-3">
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" name="username" class="form-control"
-                                    id="username" placeholder="Masukkan Username" autofocus autocomplete="off" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="fullname">Fullname</label>
-                                <input type="text" name="fullname" class="form-control"
-                                    id="fullname" placeholder="Masukkan Nama Lengkap" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" class="form-control"
-                                    id="password" placeholder="Masukkan Password" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password2">Konfirmasi Password</label>
-                                <input type="password" name="password2" class="form-control"
-                                    id="password" placeholder="Masukkan Kembali Password Anda" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="level">Level</label>
-                                <select name="level" id="level" class="form-control" required>
-                                    <option value="">-- Level User --</option>
-                                    <option value="1">Administrator</option>
-                                    <option value="2">Supervisor</option>
-                                    <option value="3">Operator</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="address">Address</label>
-                                <textarea name="address" id="address"
-                                    cols="" rows="3" class="form-control"
-                                    placeholder="masukkan alamat user" required></textarea>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="card-header">
+                        <h3 class="card-title"><i class="fas fa-user-plus fa-sm"></i> Add User</h3>
+                        <button type="submit" name="simpan" class="btn btn-primary btn-sm float-right"><i class="far fa-save"></i> Simpan</button>
+                        <button type="reset" class="btn btn-danger btn-sm float-right mr-1"><i class="fas fa-times"></i> Reset</button>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-8 mb-3">
+                                <div class="form-group">
+                                    <label for="username">Username</label>
+                                    <input type="text" name="username" class="form-control"
+                                        id="username" placeholder="Masukkan Username" autofocus autocomplete="off" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="fullname">Fullname</label>
+                                    <input type="text" name="fullname" class="form-control"
+                                        id="fullname" placeholder="Masukkan Nama Lengkap" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" name="password" class="form-control"
+                                        id="password" placeholder="Masukkan Password" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="password2">Konfirmasi Password</label>
+                                    <input type="password" name="password2" class="form-control"
+                                        id="password" placeholder="Masukkan Kembali Password Anda" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="level">Level</label>
+                                    <select name="level" id="level" class="form-control" required>
+                                        <option value="">-- Level User --</option>
+                                        <option value="1">Administrator</option>
+                                        <option value="2">Supervisor</option>
+                                        <option value="3">Operator</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <textarea name="address" id="address"
+                                        cols="" rows="3" class="form-control"
+                                        placeholder="masukkan alamat user" required></textarea>
 
-                                </select>
+                                    </select>
+                                </div>
+                                <!-- membuat kolom large8, margin bottom 3 -->
                             </div>
-                            <!-- membuat kolom large8, margin bottom 3 -->
-                        </div>
-                        <div class="col-lg-4 text-center">
-                            <img src="<?= $main_url ?>asset/image/default.png" class="profile-user-img img-circle mb-3" alt="">
-                            <input type="file" class="form-control" name="image">
-                            <span class="text-sm">Tipe file gambar JPG | PNG | GIF</span>
-                            <span class="text-sm">width = height</span>
+                            <div class="col-lg-4 text-center">
+                                <img src="<?= $main_url ?>asset/image/default.png" class="profile-user-img img-circle mb-3" alt="">
+                                <input type="file" class="form-control" name="image">
+                                <span class="text-sm">Tipe file gambar JPG | PNG | GIF</span>
+                                <span class="text-sm">width = height</span>
 
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
     </section>
     <?php
